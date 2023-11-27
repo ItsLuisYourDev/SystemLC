b74f80ad8ad50483e9c48d83a804dcf33ca257c8
luisbsc <antocede5@gmail.com>
2023-11-27 11:47:42 -0500

    update files rutas

--- router/rutasBd.js
+++ router/rutasBd.js
@@ -2,7 +2,7 @@ const { Router } = require("express")
 const router = Router()
 const metodosBd = require('../controllers/controllerBd')
 
-router.post("/insert",metodosBd.insert)
+router.post("/",metodosBd.insert)
 router.get("/",metodosBd.get)
 router.get("/:id",metodosBd.getId)
 router.delete("/:id",metodosBd.deleted)
